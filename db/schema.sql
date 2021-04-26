1c66d90d2975ca74b5651c1cb3d44c2b900e296f
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-04-26 13:37:21 -0500

    adding to schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,25 +2,27 @@ DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
-CREATE TABLE department(
+CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL,
   department_name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role(
+CREATE TABLE role (
   id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30),
-  salary DECIMAL ,
+  salary DECIMAL (10, 3),
   PRIMARY KEY (id)
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
   id INT AUTO_INCREMENT NOT NULL,
   firstName VARCHAR(30),
   lastName VARCHAR(30),
   PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES role
+  roleId INT NOT NULL,
+  managerId INT references employee,
+  FOREIGN KEY (roleId) REFERENCES role(id)
 );
 
 SELECET * FROM department;
