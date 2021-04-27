8ebaa4d025d11966397cde3b7d99df144fb6eae2
emilymetzgar93 <emilymetzgar93@gmail.com>
2021-04-27 10:54:52 -0500

    fixed connection, just using mysql, not mysql2

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,27 @@
-const Sequelize = require('sequelize');
+
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Your password
+  password: ' ',
+  database: 'employee_tracker_db',
+});
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}`);
   connection.end();
 });
 
-module.exports = sequelize;
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,4 @@
+const inquirer = require("inquirer");
+const connection = require("./config/connection.js");
+
+// function which prompts the user for what action they should take
